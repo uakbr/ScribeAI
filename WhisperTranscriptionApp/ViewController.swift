a9ee0dea284812d8e110fa5d5a5761da4e6b21ab
uakbr <umair@tesla.com.ai>
2024-11-17 09:50:41 -0600

    Initial Commit

---
+++ WhisperTranscriptionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhisperTranscriptionApp
+//
+//  Created by yu on 11/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhisperTranscriptionAppTests/WhisperTranscriptionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperTranscriptionAppTests.swift
+//  WhisperTranscriptionAppTests
+//
+//  Created by yu on 11/17/24.
+//
+
+import Testing
+@testable import WhisperTranscriptionApp
+
+struct WhisperTranscriptionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
